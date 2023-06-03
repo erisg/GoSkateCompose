ac2351469fdd9cc9936667dfdbc7d7fe9bd96f76
erisg <erisbg.513@gmail.com>
2023-06-03 00:28:06 -0500

    -Maps screen creation is finish -It must start with the creation of the modal to show the detail of the place

--- app/src/main/java/com/goskate/goskate/ui/screen/map/MapsScreen.kt
+++ app/src/main/java/com/goskate/goskate/ui/screen/map/MapsScreen.kt
@@ -2,26 +2,42 @@ package com.goskate.goskate.ui.screen.map
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.goskate.goskate.R
 import com.goskate.goskate.ui.theme.GoSkateTheme
 
 @Composable
 fun MapsScreen() {
+    val context = LocalContext.current
+    val mapProperties by remember {
+        mutableStateOf(
+            MapProperties(
+                mapStyleOptions = MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style),
+            ),
+        )
+    }
     val singapore = LatLng(4.573895909588669, -74.10398668418597)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 12f)
+        position = CameraPosition.fromLatLngZoom(singapore, 13f)
     }
 
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState,
+        properties = mapProperties,
     ) {
         Marker(
             state = MarkerState(position = singapore),
